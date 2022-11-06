36bc4887b853dee3185dd151a5e16c9b78e5a629
indresh0564 <72225782+indresh0564@users.noreply.github.com>
2022-11-06 08:08:11 +0530

    Update insertion_sort.js

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   indresh sahu
     
     *****************
 */
@@ -58,7 +58,7 @@ function Insertion()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-  indresh sahu
     
     *****************
 */
